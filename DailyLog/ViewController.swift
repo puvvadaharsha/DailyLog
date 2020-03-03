97864e6a388178bd151df90e6a8b3cf95926013d
puvvadaharsha <puvvadaharsha@gmail.com>
2020-03-03 17:48:35 -0600

    Initial Commit

---
+++ DailyLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyLog
+//
+//  Created by Harsha Puvvada on 3/3/20.
+//  Copyright © 2020 Harsha Puvvada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
