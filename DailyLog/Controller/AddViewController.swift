fd74d96fb41b13cbf8473cf11c7f0f4f1632669b
puvvadaharsha <puvvadaharsha@gmail.com>
2020-03-12 21:21:01 -0500

    Added LogViewController, AddViewController, SettingViewController and laid out objects on each view controller

---
+++ DailyLog/Controller/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  DailyLog
+//
+//  Created by Harsha Puvvada on 3/12/20.
+//  Copyright © 2020 Harsha Puvvada. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
