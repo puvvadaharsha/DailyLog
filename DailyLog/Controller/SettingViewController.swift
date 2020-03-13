f87d451c5f4f2fbd465762e525c2b8d76c4031ed
puvvadaharsha <puvvadaharsha@gmail.com>
2020-03-13 17:00:57 -0500

    Added app icon and functionality for SettingViewController: - darkmode toggle - local notification at user's selected time - reset all button

--- DailyLog/Controller/SettingViewController.swift
+++ DailyLog/Controller/SettingViewController.swift
@@ -7,24 +7,119 @@
 //
 
 import UIKit
+import UserNotifications
+
 
 class SettingViewController: UIViewController {
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    @IBOutlet weak var logItemSwitch: UISwitch!
+    @IBOutlet weak var chooseTimeLabel: UILabel!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var addReminderButton: UIButton!
+    @IBOutlet weak var resetAllButton: UIButton!
+    
+    let center = UNUserNotificationCenter.current()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //make sure reminders are hidden till user activates them
+        makeHidden(answer: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func darkModeTogglePress(_ sender: UISwitch) {
+        //get the parent viewcontroller and change its dark mode settings
+        guard let parentVC = self.parent else{return}
+        guard let parent = parentVC as? UITabBarController else{return}
+        if sender.isOn{
+            parent.overrideUserInterfaceStyle = .dark
+        } else{
+            parent.overrideUserInterfaceStyle = .light
+        }
     }
-    */
+    
+    @IBAction func reminderTogglePress(_ sender: UISwitch) {
+        //make label, timepicker and addreminder Button Appear
+        if sender.isOn {
+            makeHidden(answer: false)
+        } else{
+            makeHidden(answer: true)
+            center.removeAllPendingNotificationRequests()
+        }
+    }
+    
+    //hide the local reminder options
+    func makeHidden(answer: Bool){
+        chooseTimeLabel.isHidden = answer
+        addReminderButton.isHidden = answer
+        timePicker.isHidden =  answer
+    }
+    
+    //get time from time picker and create local notification
+    @IBAction func addReminderPress(_ sender: UIButton) {
+        //set datepickermode
+        timePicker.datePickerMode = UIDatePicker.Mode.time
+        
+        //create a dateformatter object to convert chosen time
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        let timeChosen = dateFormatter.string(from: timePicker.date)
+        
+        //create an alert controller to confirm chosen time with user
+        let alertController = UIAlertController(title: "Confirm?", message: "We will remind you daily at \(timeChosen) to update your log.", preferredStyle: .alert)
+        
+        //if user accepts, add local notification at chosen time
+        let accept = UIAlertAction(title: "Ok", style: .default, handler:{_ in self.createLocalNotification(at: self.timePicker.date)})
+        
+        //if user cancels, remove existing notification and hide the reminder part
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: {_ in
+            self.makeHidden(answer: true)
+            self.logItemSwitch.isOn = false
+        })
+        
+        //add action and present alert
+        alertController.addAction(cancel)
+        alertController.addAction(accept)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func createLocalNotification(at time: Date){
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([Calendar.Component.hour , Calendar.Component.minute], from: time)
+        let center = UNUserNotificationCenter.current()
 
+        //request permission to send local notifications
+        center.requestAuthorization(options: [.alert, .badge, .sound], completionHandler:{(granted, error) in
+            if granted{
+                //create trigger
+                let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+                
+                //create content
+                let content = UNMutableNotificationContent()
+                content.title = "DailyLog Reminder"
+                content.body = "Hey there, don't forget to log your items for today!"
+                content.sound = UNNotificationSound.default
+                
+                //add request to center
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                center.add(request)
+                
+            }else{
+                center.removeAllPendingNotificationRequests()
+            }})
+    }
+    
+    
+    //need to add action if reset is confirmed
+    @IBAction func resetAllPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Reset all Settings and Data", message: "Are you sure?", preferredStyle: .alert)
+        let accept = UIAlertAction(title: "Confirm", style: .destructive, handler: nil)
+        let cancel = UIAlertAction(title:"Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(cancel)
+        alert.addAction(accept)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
